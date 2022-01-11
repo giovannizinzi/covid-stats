d0099d6d099feb9247a3a48b0d89aa63429063cf
giovannizinzi <jzooms@gmail.com>
2022-01-11 14:07:15 -0600

    Initial Commit

---
+++ covidtracker/covidtrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  covidtrackerApp.swift
+//  covidtracker
+//
+//  Created by Giovanni Zinzi on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct covidtrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
