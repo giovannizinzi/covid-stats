70afbb298ca038fba84afbdcb5f3d64f439ea5a5
giovannizinzi <jzooms@gmail.com>
2022-01-11 14:25:17 -0600

    starting covid tracker app

--- covidtracker/ContentView.swift
+++ covidtracker/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hey we're going to make covid statistics here")
             .padding()
     }
 }
